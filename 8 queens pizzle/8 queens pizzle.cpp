2817478a3ff43d4721a59d11f850eac74e5967d4
Kaaras C <alexconoscente@icloud.com>
2020-08-28 12:00:32 -0400

    y

--- 8 queens pizzle/8 queens pizzle.cpp
+++ 8 queens pizzle/8 queens pizzle.cpp
@@ -1,24 +1,91 @@
 #include <iostream>
 using namespace std;
-char g[8][8] = { {'+','+','+'}, {'+','+','+'}, {'+','+','+'} {'+','+','+'}, {'+','+','+'}, {'+','+','+'}, {'+','+','+'}, {'+','+','+'} };
+char g[8][8];
 char queen = '*';
-int action() {
-	while (true) {
-		for (int a = 0; a < 3; a++) {
-			for (int b = 0; b < 3; b++) {
-				cout << g[a][b];
-			}
-			cout << endl;
+void make_board() {
+	for (int a = 0; a < 8; a++) {
+		for (int b = 0; b < 8; b++) {
+			g [a][b] = '+';
 		}
-
+	}
+}
+void print_board() {
+	for (int a = 0; a < 8; a++) {
+		for (int b = 0; b < 8; b++) {
+			cout << g[a][b];
+		}
+		cout << endl;
+	}
+}
+bool action(int y, int x) {
 
 		//column
-
+	for (int a = y+1; a < 8; a++) {
+		if (g[x][a] == queen) {
+			return true;
+		}
+	}
+	for (int a = y - 1; a >= 0; a--) {
+		if (g[x][a] == queen) {
+			return true;
+		}
+	}
 		//row
+	for (int b = x+1; b < 8; b++) {
+		if (g[b][y] == queen) {
+			return true;
+		}
+	}
+	for (int b = x - 1; b >= 0; b--) {
+		if (g[b][y] == queen) {
+			return true;
+		}
+	}
+		//diagonal 
 
-		//diagonal left to right
 
-		//diagonal right to left
+	//up right
+	int b = x + 1;
+	
+	for (int a = y + 1; a < 8 && b < 8; a++) {
+		if (g[b][a] == queen) {
+			return true;
+		}
+		b++;
+	}
+
+	
+	//down left
+	b = x - 1;
+
+	for(int a = y - 1; a >= 0 && b >= 0; a--) {
+		if (g[b][a] == queen) {
+			return true;
+		}
+		b--;
+	}
+
+
+
+		//up left
+	b = x - 1;
+
+	for (int a = y+1; a < 8 && b >= 0; a++) {
+		if (g[b][a] == queen) {
+			return true;
+		}
+		b--;
+	}
+
+
+	//down right
+	b = x + 1;
+
+	for (int a = y - 1; a >= 0 && b < 8; a--) {
+		if (g[b][a] == queen) {
+			return true;
+		}
+		b++;
 	}
 }
 
