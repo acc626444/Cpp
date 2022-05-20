46bc8f7d64e9af19c1c95135b9aa85b8695fe06e
Kaaras C <alexconoscente@icloud.com>
2022-05-19 22:23:06 -0400

    yo

--- Physics Final Project/functions.h
+++ Physics Final Project/functions.h
@@ -6,21 +6,47 @@ void space(int x) {
 	}
 }
 void print_def() {
-	cout << "to print to terminal, use 'cout'." << endl;
-	cout << "use '<<' between code and phrases" << endl;
-	cout << "use 'endl' to skip a line after printing" << endl;
-	cout << "use ';' to end a line of code" << endl;
+	cout << "Definition: The function of PRINT sends texts, variables, or another object to the screen." << endl << endl;
+	cout << "1) To PRINT to the screen, start by typing the word cout. cout means character output." << endl;
+	cout << "2) Directly after typing cout, you  must type << which tells the computer to display a stream of characters." << endl;
+	cout << "3) After typing cout <<, you will need to type your statement. This means here you will tell the computer what you want the code to do.";
+	cout<< "This is what the screen will PRINT. " << endl;
+	cout << "4) If you want the screen to show code on a new line after you PRINT, use << again and and endl after. This is the same thing as saying enter after the PRINT" << endl;
+	cout << "5) Type ; to end the line of code. This is used for any code. It represent period to a computer, saying 'This line of code is done.' " << endl;
+	cout << "Disclaimer: **Your statement should be incloses double quotations, however I cannot display that here because I am writing in code. Therefor, I be using single quotations**" << endl;
 	space(1);
-	cout << "the line will look like:" << endl;
-	cout << "cout << 'statement' << endl;" << endl;
+	cout << "The line should look like this:" << endl;
+	cout << "cout << 'your statement' << endl;" << endl;
 }
 void print_ex() {
-	cout << "example:" << endl;
+	cout << "example 1a (I am telling the computer to PRINT 0, skip a line, and end the code):" << endl;
 	cout << "cout << 0 << endl;" << endl;
 	space(2);
-	cout << "terminal:" << endl;
+	cout << "What you see on the screen after printing:" << endl;
 	cout << "0" << endl;
+	cout << "[Code ends here]" << endl;
+	space(1);
+	cout << "example 1b (I am telling the computer to PRINT 0 and end the code on the same line):" << endl;
+	cout << "cout << 0;" << endl;
+	space(1);
+	cout << "What you see on the screen after printing:" << endl;
+	cout << "0 [Code ends here]";
 
+	space(2);
+	cout << "example 2a (I am telling the computer to PRINT hi, skip a line, and end the code):" << endl;
+	cout << "cout << 'hi' << endl;  (NOTE: remember to use DOUBLE QUOTATIONS)" << endl;
+	space(2);
+	cout << "What you see on the screen after printing:" << endl;
+	cout << "hey" << endl;
+	cout << "[Code ends here]" << endl;
+	space(1);
+
+	cout << "example 2b (I am telling the computer to PRINT hi and end the code on the same line):" << endl;
+	cout << "cout << 'hi' << endl;  (NOTE: remember to use DOUBLE QUOTATIONS)" << endl;
+	space(2);
+	cout << "What you see on the screen after printing:" << endl;
+	cout << "hey [Code ends here]";
+	space(2);
 }
 void if_def() {
 	cout << "if is a condition" << endl;
@@ -113,4 +139,48 @@ void while_ex() {
 		cout << "hey" << endl;
 		y = 11;
 	}
+}
+void other() {
+	/*
+		CONDITIONS
+	*/
+	cout << "IF/ ELSE STATEMENT:" << endl;
+	/*
+				IF
+	*/
+	if_def();
+	space(1);
+	if_ex();
+	space(2);
+	/*
+		ELSE IF
+*/
+	elseif_def();
+	space(1);
+	elseif_ex();
+	space(2);
+	/*
+			ELSE
+	*/
+
+	else_def();
+	space(1);
+	else_ex();
+	space(3);
+	/*
+			FOR
+	*/
+	cout << "FOR LOOP:" << endl;
+	for_def();
+	space(1);
+	for_ex();
+	space(3);
+	/*
+			WHILE
+	*/
+	cout << "WHILE LOOP:" << endl;
+	while_def();
+	space(1);
+	while_ex();
+	space(3);
 }
