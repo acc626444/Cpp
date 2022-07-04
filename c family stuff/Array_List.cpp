ad4b31c1bb38438be51b1e42c80a57814cde8151
acc626444 <38765215+acc626444@users.noreply.github.com>
2022-07-04 17:23:26 -0400

    Update Array_List.cpp
    y

--- c family stuff/Array_List.cpp
+++ c family stuff/Array_List.cpp
@@ -1,43 +1,43 @@
 #include "Array_List.h"
 void Array_List::error(int n) {
-	if (n < 0 || n >= size) {
-		cout << "why" << endl;
-		exit(1);
+	if (n < 0 || n >= size) { //if number is picked that is less than 0 or larger than the array itself, do the following
+		cout << "why" << endl; //print why
+		exit(1); //exit with an error
 	}
 }
 int Array_List::get_value(int index) {
-	error(index);
-	return q[index];
+	error(index); // make sure the number used is not going to cause an error
+	return q[index]; //number used is good to go
 }
 Array_List::Array_List() {
-	q = new int[10];
+	q = new int[10]; //make a list of 10
 }
 int Array_List::add(int add) {
-	if (size >= max_size) {
-		max_size = max_size * 2;
-		int* a = new int[max_size];
-		for (int b = 0; b < size; b++) {
-			a[b] = q[b];
+	if (size >= max_size) { // if size is more than the max size, do the following
+		max_size = max_size * 2; double the size
+		int* a = new int[max_size]; //make a new array with that new size
+		for (int b = 0; b < size; b++) { 
+			a[b] = q[b]; //put those initial values in the new array
 		}
-		q = a;
+		q = a; //make the new array the default
 	}
-	q[size] = add;
-	size++;
+	q[size] = add; //add the new number
+	size++; //increase the size
 	return 0;
 }
 void Array_List::print_list() {
 	for (int a = 0; a < size; a++) {
-		cout << q[a] << endl << endl;
+		cout << q[a] << endl << endl; //print the array
 	}
 }
 int Array_List::remove(int remove) {
-	error(remove);
-	int s = q[remove];
+	error(remove); //check if you can remove something
+	int s = q[remove]; //save new array
 	for (int a = remove; a < size; a++) {
-		q[a] = q[a + 1];
+		q[a] = q[a + 1]; //make new array
 	}
-	size--;
-	return s;
+	size--; //reduce size
+	return s; //make new as default
 }
 void Array_List::change(int change, int n1) {
 	error(change);
