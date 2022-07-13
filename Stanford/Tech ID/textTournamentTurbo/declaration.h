7142a0f71a901c26dc1875f299b4aa21209a557b
Kaaras C <alexconoscente@icloud.com>
2022-07-12 20:38:39 -0700

    finished RPG

--- Stanford/Tech ID/textTournamentTurbo/declaration.h
+++ Stanford/Tech ID/textTournamentTurbo/declaration.h
@@ -3,5 +3,79 @@
 #include <string>
 #include <random>
 using namespace std;
-void setupPlayerStats (string& name, int& str, int& def, int& armor, int& skill, int& wins);
-void savePlayerStats (string name, int str, int def, int armor, int skill, int wins);
+
+void skip (int a){
+	for (int b = 0; b < a; b++){
+		cout << endl;
+	}
+}
+void setupPlayerStats (string& name, int& str, int& def, int& armor, int& skill, int& wins){
+	fstream file;
+	cout << "Name: ";
+	getline (cin, name);
+	if (name == ""){
+		name = "No Name\n";
+	}
+	string filename = name + ".txt";
+	file.open (filename, ios::in);
+	if (file.is_open ()){
+		file >> str;
+		file >> def;
+		file >> armor;
+		file >> skill;
+		file >> wins;
+		file.close ();
+	} else{
+		bool accepted = false;
+		while (accepted == false){
+			int points = 20;
+			cout << "you have " << points << " points assigned to character stats." << endl;
+			cout << "Stats include strength, defense, armor & skill." << endl;
+			//strength
+			cout << "  Strength: ";
+			cin >> str;
+			points -= str;
+			cout << "    You have " << points << " points remaining " << endl;
+
+			//defense
+			cout << "  defense: ";
+			cin >> def;
+			points -= def;
+			cout << "    You have " << points << " points remaining " << endl;
+
+			//Armor
+			cout << "  Armor: ";
+			cin >> armor;
+			points -= armor;
+			cout << "    You have " << points << " points remaining " << endl;
+
+			//Skill
+			cout << "  Skill: ";
+			cin >> skill;
+			points -= skill;
+			cout << "    You have " << points << " points remaining " << endl;
+
+			cin.ignore ();
+			wins = 0;
+			if (points >= 0){
+				cout << "the entered stats have been accepted. " << name << " is being created!" << endl;
+				accepted = true;
+			} else{
+				cout << "Sorry, you've exceeded the stat points limit.";
+				skip (2);
+				cout << "Try agaion...";
+			}
+		}
+	}
+}
+void savePlayerStats (string name, int str, int def, int armor, int skill, int wins){
+	fstream file;
+	string filename = name + ".txt";
+	file.open (filename, ios::out);
+	file << "strength: " << str << endl;
+	file << "defense: " << def << endl;
+	file << "armor: " << armor << endl;
+	file << "skill: " << skill << endl;
+	file << "win: " << wins << endl;
+	file.close ();
+}
--- Stanford/Tech ID/textTournamentTurbo/main.cpp
+++ Stanford/Tech ID/textTournamentTurbo/main.cpp
@@ -1,36 +1,33 @@
 ﻿#include "declaration.h"
-void skip (int a){
-	for (int b = 0; b < a; b++){
-		cout << endl;
-	}
-}
 
+void setupPlayerStats (string& name, int& str, int& def, int& armor, int& skill, int& wins);
+void savePlayerStats (string name, int str, int def, int armor, int skill, int wins);
 int main (){
 	random_device rd;
 	uniform_int_distribution <int> d6 (1, 6);
 	///player 1
 	string name1;
-	int Str1;
-	int Def1;
-	int Armor1;
-	int Skill1;
-	int Wins1;
+	int Str1 = 0;
+	int Def1 = 0;
+	int Armor1 = 0;
+	int Skill1 = 0;
+	int Wins1 = 0;
 	skip (2);
-	cout << "SETTING UP PLAYER 1..." << endl << "+";
-	savePlayerStats (name1, Str1, Def1, Armor1, Skill1, Wins1);
+	cout << "SETTING UP PLAYER 1..." << endl << "+ ";
+	setupPlayerStats (name1, Str1, Def1, Armor1, Skill1, Wins1);
 	//player 2
-	string name;
-	int Str;
-	int Def;
-	int Armor;
-	int Skill;
-	int Wins;
-	skip (2);
-	cout << "SETTING UP PLAYER 2..." << endl << "=";
-	savePlayerStats (name, Str, Def, Armor, Skill, Wins);
 	int Health1 = 20;
 	int Health = 20;
 	int turns = 0;
+	string name;
+	int Str = 0;
+	int Def = 0;
+	int Armor = 0;
+	int Skill = 0;
+	int Wins = 0;
+	skip (2);
+	cout << "SETTING UP PLAYER 2..." << endl << "0 ";
+	setupPlayerStats (name, Str, Def, Armor, Skill, Wins);
 	//announce
 	cout << endl << "TIME TO FIGHT" << endl;
 	cout << "Introducing the challenge..." << endl;
@@ -41,9 +38,26 @@ int main (){
 		int attackRoll;
 		int damageRoll;
 		cout << " Begin Round " << turns + 1 << endl;
-		attackRoll >= Skill1 + d6 (rd);
-		if (attackRoll + 3 + Def){
-			cout << name1 << "hits " << "..." << endl;
+		//player 1's turn
+		attackRoll = Skill1 + d6 (rd);
+		//damage
+		if (attackRoll >= 3 + Def){
+			cout << name1 << " hits " <<name<< "..." << endl;
+			damageRoll = Str1 + d6 (rd) - Armor;
+			if (damageRoll > 0){
+				Health -= damageRoll;
+				cout << ".." << name << " takes " << damageRoll << " damage!" << endl;
+			} else{
+				cout << "..." << name << "'s armor blocks all the damage!" << endl;
+			}
+		} else{
+			cout << name1 << " swings at" << name << "..." << endl;
+			cout << "..." << name << "'s defense blocks the hit" << endl;
+		}//player 1's turn
+		attackRoll = Skill1 + d6 (rd);
+		//damage
+		if (attackRoll >= 3 + Def){
+			cout << name1 << " hits " <<name<< "..." << endl;
 			damageRoll = Str1 + d6 (rd) - Armor;
 			if (damageRoll > 0){
 				Health -= damageRoll;
@@ -55,94 +69,41 @@ int main (){
 			cout << name1 << " swings at" << name << "..." << endl;
 			cout << "..." << name << "'s defense blocks the hit" << endl;
 		}
+		//player 2's turn
+		attackRoll = Skill + d6 (rd);
+		//damage
+		if (attackRoll >= 3 + Def1){
+			cout << name << " hits " << name1 << "..." << endl;
+			damageRoll = Str + d6 (rd) - Armor1;
+			if (damageRoll > 0){
+				Health1 -= damageRoll;
+				cout << ".." << name1 << " takes " << damageRoll << " damage!" << endl;
+			} else{
+				cout << "..." << name1 << "'s armor blocks all the damage!" << endl;
+			}
+		} else{
+			cout << name << " swings at" << name1 << "..." << endl;
+			cout << "..." << name1 << "'s defense blocks the hit" << endl;
+		}
+		//health
 		cout << "ROUND RESULTS... " << endl;
-		cout << "0 " << name1 << " 's Health: " << Health1 << endl;
-		cout << "0 " << name << " 's Health: " << Health << endl << endl;
+		cout << "+ " << name1 << "'s Health: " << Health1 << endl;
+		cout << "0 " << name << "'s Health: " << Health << endl << endl;
 		turns++;
 	}
+	//winner
 	if (((Health1 > 0) && (Health > 0)) || ((Health1 <= 0) && (Health <= 0))){
 		cout << "THE RESULTS ARE IN..." << endl;
 		cout << "...the fight was so close that it ended in a DRAW!" << endl;
 	} else if (Health1 > 0){
 		cout << "THE RESULTS ARE IN..." << endl;
-		cout << "...it look like" << name1 << "pulled off the win" << endl;
+		cout << "...it look like " << name1 << " pulled off the win" << endl;
 		Wins1++;
 	} else{
 		cout << "THE RESULTS ARE IN..." << endl;
-		cout << "...it look like" << name << "pulled off the win" << endl;
+		cout << "...it look like " << name << " pulled off the win" << endl;
 		Wins++;
 	}
 	savePlayerStats (name1, Str1, Def1, Armor1, Skill1, Wins1);
 	savePlayerStats (name, Str, Def, Armor1, Skill, Wins);
-}
-void setupPlayerStats (string& name, int& str, int& def, int& armor, int& skill, int& wins){
-	fstream file;
-	cout << "Name: ";
-	getline (cin, name);
-	if (name == ""){
-		name = "No Name\n";
-	}
-	string filename = name + ".txt";
-	file.open (filename, ios::in);
-	if (file.is_open ()){
-		file >> str;
-		file >> def;
-		file >> armor;
-		file >> skill;
-		file >> wins;
-		file.close ();
-	} else{
-		bool accepted = false;
-		while (accepted == false){
-			int points = 20;
-			cout << "you have " << points << "points assing to character stats." << endl;
-			cout << "Stats include strength, defense, armor & skill." << endl;
-			//strength
-			cout << "  ・Strength";
-			cin >> str;
-			points - +str;
-			cout << "    You have " << points << "points remaining " << endl;
-
-			//defense
-			cout << "  ・defense";
-			cin >> def;
-			points - +def;
-			cout << "    You have " << points << "points remaining " << endl;
-
-			//Armor
-			cout << "  ・Armor";
-			cin >> armor;
-			points - +armor;
-			cout << "    You have " << points << "points remaining " << endl;
-
-			//Skill
-			cout << "  ・Skill";
-			cin >> skill;
-			points - +skill;
-			cout << "    You have " << points << "points remaining " << endl;
-
-			cin.ignore ();
-			wins = 0;
-			if (points >= 0){
-				cout << "the entered stats have been accepted. " << name << "is being created!" << endl;
-				accepted = true;
-			} else{
-				cout << "Sorry, you've exceeded the stat points limit.";
-				skip (2);
-				cout << "Try agaion...";
-			}
-		}
-	}
-}
-void savePlayerStats (string name, int str, int def, int armor, int skill, int wins){
-	fstream file;
-	string filename = name + ".txt";
-	file.open ("test.txt", ios::out);
-	if (file.is_open ());
-		file >> str;
-		file >> def;
-		file >> armor;
-		file >> skill;
-		file >> wins;
-		file.close ();
 }
