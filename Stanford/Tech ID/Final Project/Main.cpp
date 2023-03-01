21b50cd6d5f1e3ae55657912406d4a0e6e25c05d
acc626444 <alexconoscente@icloud.com>
2023-03-01 16:38:39 -0500

    yp

--- Stanford/Tech ID/Final Project/Main.cpp
+++ Stanford/Tech ID/Final Project/Main.cpp
@@ -1,20 +1,22 @@
 #pragma warning(disable : 4996)
-#include "EndConditions.h"
+#include "Again.h"
 int main() {
-	sf::RenderWindow window(sf::VideoMode(1600, 800), "Black Jack!");
-	sf::Event event;
-	srand(time(0));
-	
-	Deck d;
-	d.Shuffle();
-	Hand name(&d);
-	Hand name2(&d);
-	player1(name, window);
-	//sf::sleep(sf::seconds(2));
-	
-	dealer1(name2, window);
-	//sf::sleep(sf::seconds(2));
+	bool game = true;
+	while (game) {
+		sf::RenderWindow window(sf::VideoMode(1600, 800), "Black Jack!");
+		sf::Event event;
+		srand(time(0));
 
+		Deck d;
+		d.Shuffle();
+		Hand name(&d);
+		Hand name2(&d);
+		//name.handValue() = 0;
+		player1(name, window);
+		//sf::sleep(sf::seconds(2));
+		dealer1(name2, window);
+		//sf::sleep(sf::seconds(2));
+		EndCondition(name, name2, window);
 
-	EndCondition(name, name2, window);
+	}
 }
