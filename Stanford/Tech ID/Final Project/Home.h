ad11f14741a6b67e5a84ffb8c707dccf83bfe530
acc626444 <alexconoscente@icloud.com>
2023-06-12 15:39:28 -0400

    done

--- Stanford/Tech ID/Final Project/Home.h
+++ Stanford/Tech ID/Final Project/Home.h
@@ -1,45 +1,5 @@
 #pragma once
 #include "Again.h"
-sf::Font buttonFont;
-sf::Text Home;
-sf::Text play;
-void home(sf::RenderWindow& w) {
-	sf::Event event;
-	font.loadFromFile("japanese-style-font/JapaneseStyle-rmX7.ttf");
-	Home.setFont(font);
-	Home.setString("BLACKJACK");
-	Home.setPosition(400, -50);
-	Home.setCharacterSize(200);
-	sf::Color pinkish(55, 25, 255);
-
-	Button Play("Start", { 100, 100 }, 60, sf::Color::Transparent, pinkish);
-	buttonFont.loadFromFile("japanese-style-font/JapaneseStyle-rmX7.ttf");
-	Play.Font(buttonFont);
-	Play.position({ 50, 650 });
-	while(true) {
-		while (w.pollEvent(event)) {
-			switch (event.type) {
-			case sf::Event::Closed:
-				w.close();
-				exit(0);
-			case sf::Event::MouseMoved:
-				if (Play.mouseOver(w)) {
-					sf::sleep(sf::milliseconds(2));
-					Play.TextColour(sf::Color::Green);
-				}
-				if (!(Play.mouseOver(w))) {
-					sf::sleep(sf::milliseconds(2));
-					Play.TextColour(pinkish);
-				}
-			}
-			w.clear(sf::Color::Red);
-			Play.drawTo(w);
-			w.draw(Home);
-
-			w.display();
-		}
-	}
-}
 
 void PlayButton(sf::RenderWindow& w) {
 	bool game = true;
@@ -57,3 +17,47 @@ void PlayButton(sf::RenderWindow& w) {
 		game = continu(name, name2, w, game);
 	}
 }
+void home(sf::RenderWindow& w) {
+	sf::Font buttonFont;
+	sf::Text Home;
+	sf::Text play;
+	sf::Event event;
+	font.loadFromFile("japanese-style-font/JapaneseStyle-rmX7.ttf");
+	Home.setFont(font);
+	Home.setString("BLACKJACK");
+	Home.setPosition(400, -50);
+	Home.setCharacterSize(200);
+	sf::Color pinkish(55, 25, 255);
+
+	Button Play("Start", { 150, 59 }, 60, sf::Color::Transparent, pinkish);
+	buttonFont.loadFromFile("japanese-style-font/JapaneseStyle-rmX7.ttf");
+	Play.Font(buttonFont);
+	Play.position({ 725, 270 });
+	while(true) {
+		while (w.pollEvent(event)) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				w.close();
+				exit(0);
+			case sf::Event::MouseMoved:
+				if (Play.mouseOver(w)) {
+					sf::sleep(sf::milliseconds(2));
+					Play.TextColour(sf::Color::Green);
+				}
+				if (!(Play.mouseOver(w))) {
+					sf::sleep(sf::milliseconds(2));
+					Play.TextColour(pinkish);
+				}
+			case sf::Event::MouseButtonPressed:
+				if (Play.mouseOver(w) && sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+					PlayButton(w);
+				}
+			}
+			w.clear(sf::Color::Red);
+			Play.drawTo(w);
+			w.draw(Home);
+			w.display();
+		}
+	}
+}
+
--- Stanford/Tech ID/Final Project/Main.cpp
+++ Stanford/Tech ID/Final Project/Main.cpp
@@ -2,7 +2,6 @@
 #include "Home.h"
 #include"cmath";
 int main() {
-		/*sf::RenderWindow window(sf::VideoMode(1600, 800), "Black Jack!");
-		home(window);*/
-	cout << pow(2,4);
+		sf::RenderWindow window(sf::VideoMode(1600, 800), "Black Jack!");
+		home(window);
 }
